a03528f45572d99376017e2a1fbd49b881a6e67a
Miłosz Sędziak <m.sedziak01@gmail.com>
2018-05-18 00:29:19 +0200

    Bind and fuction to bind value

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState(
         {
           persons: [
-            { name: "Paweł", age: 20},
+            { name: newName, age: 20},
             { name: "Miłosz", age: 20},
             { name: "Natalia", age: 20},
           ]
@@ -29,10 +29,18 @@ class App extends Component {
       <div className="App">
         <h1>Andrzej eat glass. Why?</h1>
         <p>Hi, it working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I love swimming! </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'Andrzejek!')}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Andrzejekejekejek!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}> I love swimming! </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}/>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
       </div>
     );
   }
